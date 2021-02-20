ee140f3b861f4d27a750efa8f79abeaeb89df0b1
bones-bones <elliot.ma.brown@gmail.com>
2021-02-20 00:39:12 -0800

    log match creation and game state events

--- src/gameLogListener.ts
+++ src/gameLogListener.ts
@@ -11,10 +11,22 @@ export const constructLogEventHandler = (activeLogFile: string) => {
                 start: currentIndex
             }).on('data', (data) => {
                 readData += data;
+                console.log(data.length, 'is data')
             }).on('error', (err) => {
                 console.error(err)
             }).on("end", () => { resolve(readData); })
         }));
         currentIndex += newValue.length;
+
+        console.log(newValue.length)
+
+        if (newValue.includes('Event.MatchCreated')) {
+            console.log("Match has been created!");
+        }
+        if (newValue.includes('GREMessageType_GameStateMessage')) {
+            console.log("Game State Event!");
+            console.log(newValue);
+        }
     }
 }
+// cool events
