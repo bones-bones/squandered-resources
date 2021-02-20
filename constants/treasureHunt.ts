e02d6ad811e16a9e2f170c270f418e93e04465c2
bones-bones <elliot.ma.brown@gmail.com>
2021-02-19 21:19:45 -0800

    Initial commit

---
+++ constants/treasureHunt.ts
@@ -0,0 +1,8 @@
+/**
+ * Extra idiot mode.
+ * Could use cycling lands but that would require more decision points.
+ */
+export const BASIC_MIRAGE_ISLAND_NAME = 652;    // 52x I'm too poor to afford the pokemon lands
+export const DIG_FOR_TREASURE_NAME = 24877;     // 3x
+export const MYSTIC_SANCTUARY_NAME = 414466;    // 4x
+export const THASSAS_ORACLE_NAME = 419870;      // 1x
